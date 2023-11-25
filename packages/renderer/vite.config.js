d671b1b19257824c47634f18230b022772bac8b3
bangvc <bangvc@vgroup.vn>
2023-11-25 16:19:21 +0700

    port to react and pnpm

--- packages/renderer/vite.config.js
+++ packages/renderer/vite.config.js
@@ -1,10 +1,11 @@
 /* eslint-env node */
 
 import {chrome} from '../../.electron-vendors.cache.json';
-import vue from '@vitejs/plugin-vue';
+import react from '@vitejs/plugin-react';
 import {renderer} from 'unplugin-auto-expose';
 import {join} from 'node:path';
 import {injectAppVersion} from '../../version/inject-app-version-plugin.mjs';
+import importCommonPlugin from '../../plugins/import-common-plugin.js';
 
 const PACKAGE_ROOT = __dirname;
 const PROJECT_ROOT = join(PACKAGE_ROOT, '../..');
@@ -43,7 +44,10 @@ const config = {
     environment: 'happy-dom',
   },
   plugins: [
-    vue(),
+    react(),
+    importCommonPlugin({
+      commonEntry: join(PACKAGE_ROOT, '../common/dist/index.js'),
+    }),
     renderer.vite({
       preloadEntry: join(PACKAGE_ROOT, '../preload/src/index.ts'),
     }),
